e8796dca194825b0cca6d90aa901658e46e1f25b
DureginKostya <kot-502@mail.ru>
2023-10-26 10:27:25 +0500

    Changed method test_guest_should_see_login_form_and_register_forminto file test_main_page.py (added cross reference)

--- thesis_project/test_main_page.py
+++ thesis_project/test_main_page.py
@@ -17,7 +17,10 @@ def test_guest_should_see_login_link(browser):
 
 
 def test_guest_should_see_login_form_and_register_form(browser):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
-    page = LoginPage(browser, link)
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    login_page.should_be_login_page()
