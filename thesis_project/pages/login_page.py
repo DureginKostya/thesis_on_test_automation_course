275331ba8ab9abfd79ff2de655bc8b9466db2e40
DureginKostya <kot-502@mail.ru>
2023-10-29 16:11:09 +0500

    Made final codereview

--- thesis_project/pages/login_page.py
+++ thesis_project/pages/login_page.py
@@ -26,7 +26,6 @@ class LoginPage(BasePage):
         button_registration.click()
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form is not presented'
 
     def should_be_login_page(self):
@@ -35,9 +34,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert 'login' in self.browser.current_url, 'Error: Invalid link'
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Register form is not presented'
--- thesis_project/test_product_page.py
+++ thesis_project/test_product_page.py
@@ -17,6 +17,7 @@ class TestUserAddToBasketFromProductPage():
         self.login_page.register_new_user(email, '!!123456789!!')
         self.login_page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
         page = ProductPage(browser, link)
@@ -45,6 +46,7 @@ class TestUserAddToBasketFromProductPage():
                               , marks=pytest.mark.xfail),
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
@@ -53,13 +55,15 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_messages_product(name, price)
 
 
-def test__can_go_to_login_page_from_product_page(browser):
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     page = ProductPage(browser, link)
